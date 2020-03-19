04e35c4266ca8bd4f28f4e9c0c2817b09c5978a8
Psycho <laurentchervet@bluewin.ch>
2020-03-19 23:02:52 +0100

    First step for stdout markdown support

--- Reminder.py
+++ Reminder.py
@@ -1,4 +1,5 @@
 from core.base.model.AliceSkill import AliceSkill
+from core.base.model.Intent import Intent
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import IntentHandler
 
@@ -9,6 +10,33 @@ class Reminder(AliceSkill):
 	Description: Ask alice to remind you stuff at a given time
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
+	DATABASE = {
+		'reminders': [
+			'id INTEGER PRIMARY KEY',
+			'added INTEGER NOT NULL',
+			'remindAt INTEGER NOT NULL',
+			'reminder TEXT NOT NULL'
+		]
+	}
+
+	_INTENT_ADD_REMINDER = Intent('AddReminder')
+	_INTENT_RANDOM_ANSWER = Intent('RandomAnswer')
+
+
+	def __init__(self):
+		self._INTENTS = [
+			(self._INTENT_ADD_REMINDER, self.addReminder)
+		]
+		super().__init__(self._INTENTS, self.DATABASE)
+
+
+	def addReminder(self, session: DialogSession, **_kwargs):
+		self.continueDialog(
+			sessionId=session.sessionId,
+			text=self.randomTalk(text='aboutWhat')
+		)
+
+
+	@IntentHandler('UserRandomAnswer')
+	def answerReminder(self, session: DialogSession, **_kwargs):
 		pass
